9f6eb25227ab11aa0ab015e87add9cc59475f7ab
Joseph Ofem <impaktek@gmail.com>
2025-02-04 20:55:41 +0100

    v1.0.0

--- impakprint/build.gradle.kts
+++ impakprint/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.maven.publish)
+    id("maven-publish")
 }
 
 afterEvaluate { // ✅ Ensures components["release"] exists
